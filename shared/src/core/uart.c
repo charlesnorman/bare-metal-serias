8a1c2c9a6a1c1382b4d837b0a6bed26956458c76
Charles Norman, Sr <charlesrichardnorman@gmail.com>
2024-05-06 11:54:49 -0400

    Interupt Safe Ring Buffer

--- shared/src/core/uart.c
+++ shared/src/core/uart.c
@@ -3,29 +3,33 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/usart.h>
 #include "core/uart.h"
+#include "core/ring_buffer.h"
 
 #define BAUD_RATE (115200)
 #define DATA_BITS (8)
+#define RING_BUFFER_SIZE (128)
 
-static uint8_t data_buffer = 0U;
-static bool data_available = false;
+static ring_buffer_t rb = {0U};
+static uint8_t data_buffer[RING_BUFFER_SIZE] = {0U};
 
 void usart1_isr(void)
 {
-    const uint8_t flags = USART1_SR;
-
+    const volatile uint8_t flags = USART1_SR;
     const bool received_data = flags & USART_FLAG_RXNE;
     const bool overrun_occurred = flags & USART_FLAG_ORE;
 
     if (received_data || overrun_occurred)
     {
-        data_buffer = (uint8_t)usart_recv(USART1);
-        data_available = true;
+        if (ring_buffer_write(&rb, (uint8_t)usart_recv(USART1)))
+        {
+            __asm__("nop"); // Handle the over write
+        }
     }
 }
 
 void uart_setup(void)
 {
+    ring_buffer_setup(&rb, data_buffer, RING_BUFFER_SIZE);
     rcc_periph_clock_enable(RCC_USART1);
 
     usart_set_mode(USART1, USART_MODE_TX_RX);
@@ -54,24 +58,31 @@ void uart_write_byte(uint8_t data)
     usart_send_blocking(USART1, (uint16_t)data);
 }
 
-uint32_t uart_read(uint8_t *data, const uint8_t lengthl)
+uint32_t uart_read(uint8_t *data, const uint8_t length)
 {
-    if (lengthl > 0 && data_available)
+    if (length == 0)
     {
-        *data = data_buffer;
-        data_available = false;
-        return 1;
+        return 0;
     }
-    return 0;
+    for (uint32_t bytes_read = 0; bytes_read < length; bytes_read++)
+    {
+        if (!ring_buffer_read(&rb, &data[bytes_read]))
+        {
+            return bytes_read;
+        }
+    }
+
+    return length;
 }
 
 uint8_t uart_read_byte(void)
 {
-    data_available = false;
-    return data_buffer;
+    uint8_t byte = 0;
+    (void)uart_read(&byte, 1);
+    return byte;
 }
 
 bool uart_data_available(void)
 {
-    return data_available;
+    return !ring_buffer_empty(&rb);
 }
